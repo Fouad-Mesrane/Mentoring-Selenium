3904ef9827cf249cc264c24b09c27e8caf9c1b60
Fouad Mesrane <fouadmesrane@gmail.com>
2022-11-29 17:35:09 -0700

    first init

---
+++ base/src/main/java/utils/TestDataReader.java
@@ -0,0 +1,36 @@
+package utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestDataReader {
+
+
+    public static Properties properties;
+    private static final File propertiesFile = new File(System.getProperty("user.dir") + File.separator + "src"
+            + File.separator + "main" + File.separator + "resources" + File.separator + "data" + File.separator
+            + "testData.properties");
+
+
+
+    public static Properties getTestData(){
+        try {
+            properties = loadProperties();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
+    private static Properties loadProperties() throws IOException {
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream(propertiesFile);
+
+        prop.load(fis);
+        fis.close();
+
+        return prop;
+    }
+}
